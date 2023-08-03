728959f2ce77cda97bcdb96a9d7bf2e95bff200b
Makhluk Tuhan Paling Seksi <virdi@l-essential.com>
2023-08-03 11:11:35 +0700

    Update komen

--- scan_port.bat
+++ scan_port.bat
@@ -1,5 +1,7 @@
 @echo off
 
+@REM Scan port website dengan OP-nya, simpan result-nya ke file .log
+
 @REM Timeout 0.001 detik
 SET CURL_TIMEOUT="0.001"
 @REM IP Server
@@ -8,12 +10,12 @@ SET IPServer="http://"
 for /l %%x in (1, 1, 65535) do (
 	curl -X GET --connect-timeout %CURL_TIMEOUT% "%IPServer%:%%x"
 	if errorlevel 28 (
-		echo "Curl timed out. Port %%x" >> output_scan.txt
+		echo "Curl timed out. Port %%x" >> output_scan.log
 	) else (
-		echo "Curl completed successfully. Port %%x" >> output_scan.txt
+		echo "Curl completed successfully. Port %%x" >> output_scan.log
 	)
 )
 
-echo "Header:" >> output_scan.txt
-curl -v -L -X HEAD --connect-timeout %CURL_TIMEOUT% "%IPServer%" >> output_scan.txt
+echo "Header:" >> output_scan.log
+curl -v -L -X HEAD --connect-timeout %CURL_TIMEOUT% "%IPServer%" >> output_scan.log
 @pause
